6697f12b0cce2975475f4fffc0c86e7167932d71
Abdel-Rahman Shaddad <codonadesh@gmail.com>
2026-09-18 16:14:07 +0300

    begin listening for connections

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # 🍽️ Silver Platter
 
 Your web application served on a silver platter by C99.
+
+## 🌐 References
+
+- [Minimalist Web Server in C](https://youtu.be/2HrYIl6GpYg?si=CUYLSaqLs5jfx_H7)
+- [A Simple Web Server Written in C](https://medium.com/@nipunweerasiri/a-simple-web-server-written-in-c-cf7445002e6)
--- main.c
+++ main.c
@@ -1,6 +1,29 @@
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <sys/socket.h>
 
 int main(void) {
-    printf("What are you doing here, web developers?\n");
+    // - create an IPv4 socket using a reliable, connection-oriented byte stream (TCP).
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
+
+    // - assign an address to the socket.
+    struct sockaddr_in socket_address = {
+        .sin_family = AF_INET,
+        .sin_port = htons(3000),
+        .sin_addr = {
+            .s_addr = htonl(INADDR_LOOPBACK),
+        },
+    };
+    bind(socket_fd, (struct sockaddr *)&socket_address, sizeof(socket_address));
+
+    // - begin listening for connections.
+    listen(socket_fd, 256);
+    char host_name[NI_MAXHOST], service_name[NI_MAXSERV];
+    getnameinfo((struct sockaddr *)&socket_address, sizeof(socket_address),
+            host_name, sizeof(host_name), service_name, sizeof(service_name), 0);
+    printf("Listening on http://%s:%s\n", host_name, service_name);
+
     return 0;
 }
